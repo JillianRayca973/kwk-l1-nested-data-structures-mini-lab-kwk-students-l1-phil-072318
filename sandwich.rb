1208cd11224958c2f0b4d68ea0bf0be07b1196d3
Jill  Rayca <rkrayca@yahoo.com>
2018-07-26 13:56:28 +0000

    automatically backed up by learn

--- sandwich.rb
+++ sandwich.rb
@@ -1,9 +1,9 @@
 ingredients = {
     ["rye", "sourdough", "baguette"]=>
     ["ham", "salami", "turkey"],
-    ["swiss", "munster", "cheddar"],
+    ["swiss", "munster", "cheddar"]=>
     ["mayo", "mustard", "tabasco"]
-  end-of-input
+  end
   
   puts "Your order is #{[0][2]},#{[1][2]},#{[2][0]},#{[3][0]"
 
