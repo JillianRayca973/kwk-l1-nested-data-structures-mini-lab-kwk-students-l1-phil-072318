305453a78549583376c4c545bb0bbef5f25b9afa
Jill  Rayca <rkrayca@yahoo.com>
2018-07-26 14:01:16 +0000

    automatically backed up by learn

--- sandwich.rb
+++ sandwich.rb
@@ -1,8 +1,8 @@
 ingredients = {
     :breads => ["rye", "sourdough", "baguette"]
-     } :meats => ["ham", "salami", "turkey"]
-    } :Cheeses => ["swiss", "munster", "cheddar"]
-    } :Condiments => ["mayo", "mustard", "tabasco"]
+      :meats => ["ham", "salami", "turkey"]
+     :Cheeses => ["swiss", "munster", "cheddar"]
+     :Condiments => ["mayo", "mustard", "tabasco"]
 }
   
   puts "Your order is #{[:breads][2]},#{[:meats][2]},#{[:Cheeses][0]},#{[:Condiments][0]"
